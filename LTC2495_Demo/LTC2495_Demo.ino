1bf91e4ef33717f1fa88a7eea9defba8e1725a0f
Bobby Schulz <schu3119@umn.edu>
2017-10-19 15:59:47 -0500

    Inital population, clean up required!

---
+++ LTC2495_Demo/LTC2495_Demo.ino
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include "LTC2495.h"
+
+LTC2495 adc = LTC2495(0x45, 5.0);
+void setup() {
+  Serial.begin(9600);
+  // put your setup code here, to run once:
+  Wire.begin();
+  adc.Begin();
+  Serial.println(HEX);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  float Val = 0;
+  Val = adc.GetVoltageDiff(0, 1);
+  Serial.print("ADC 0 = ");
+  Serial.println(Val, 5);
+  Serial.println(Val + 2.5, 5);
+  delay(500); 
+
+}
